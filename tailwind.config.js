aa7be367820d2c91742e12b51d82d07b221c2384
Rath Bala <rath.balas1@gmail.com>
2024-08-08 07:54:47 +0100

    WIP benefits. TODO - finish figma image for 4th benefit i.e. menu.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html", "./src/assets/js/**/*.js"],
+  content: ['./src/**/*.html', './src/assets/js/**/*.js'],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-to-b-green":
-          "linear-gradient(to bottom, #04bf68 0%, #04bf68 100%)",
+        'gradient-to-b-green':
+          'linear-gradient(to bottom, #04bf68 0%, #04bf68 100%)',
       },
       colors: {
-        "blue-dark": "#0b1930",
-        "green-light": "#96f294",
-        green: "#04bf68",
+        'blue-dark': '#0b1930',
+        'blue-light': '#4B92FE',
+        'green-light': '#96f294',
+        green: '#04bf68',
       },
       fontFamily: {
-        sans: ["Merriweather Sans", "sans-serif"],
-        serif: ["Roboto", "serif"],
+        sans: ['Merriweather Sans', 'sans-serif'],
+        serif: ['Roboto', 'serif'],
       },
       height: {
-        "1/2vh": "50vh",
+        '1/2vh': '50vh',
       },
       spacing: {
-        "20vh": "20vh",
-        "1rem": "1rem",
-        "1.5rem": "1.5rem",
-        "2rem": "2rem",
-        "10rem": "10rem",
+        '20vh': '20vh',
+        '1rem': '1rem',
+        '1.5rem': '1.5rem',
+        '2rem': '2rem',
+        '10rem': '10rem',
       },
     },
   },
   plugins: [],
-};
+}
