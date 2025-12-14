fee8cb42d3a46a67b4e2e36b549dc028e90a2b9a
RathBala <86238954+RathBala@users.noreply.github.com>
2025-12-14 13:01:21 +0000

    feat: Implement dark mode and theme toggle (#10)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.html', './src/**/*.js'],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
